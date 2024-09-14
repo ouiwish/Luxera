1bc0f4484bf6bf34e0b3cfb72c82f18166cec498
ouiwish <widad.moumkine@gmail.com>
2024-09-15 00:03:48 +0400

    Adding and committing AuthController.php

---
+++ Backend-Luxera/app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // Check if the user is authenticated and is an admin
+        if (Auth::check() && Auth::user()->is_admin) {
+            return $next($request);
+        }
+
+        // If not, return a 403 Forbidden response
+        return response()->json(['error' => 'Unauthorized, Access Denied'], 403);
+    }
+}
